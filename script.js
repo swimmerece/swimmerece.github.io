7e8226669bdf43059959fe48f0419080e2d25363
Ece <eceeyurtseven@gmail.com>
2024-08-11 15:25:39 +0300

    added new pages

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('flierElement').addEventListener('click', function() {
+      window.location.href = 'home.html';
+    });
+  });
+  
