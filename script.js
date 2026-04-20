c3c1b8b739a203c2223bd734a8aa390bd43d107f
Ece Yurtseven <eceeyurtseven@gmail.com>
2026-04-20 21:36:21 +0300

    minor fixes

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('flierElement').addEventListener('click', function() {
       window.location.href = 'home.html';
     });
+
+    document.querySelector('.flier-v2 img').addEventListener('click', function() {
+      window.location.href = 'https://allpoetry.com/mad-girl%27s-love-song';
+    });
   });
   
